d770d4bd5a3a29e0ceb539fd5a3a22f38930c028
John Lloyd A. Jasmines <jasminesj37@gmail.com>
2022-04-27 11:56:48 +0800

    First Commit

---
+++ My name/Programming1_FullNameOnAMessageBox/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Programming1_FullNameOnAMessageBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Stencil", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(150, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 88);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "My Name";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 320);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Full Name";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button button1;
+    }
+}
---
+++ My name/Programming1_FullNameOnAMessageBox/Form1.cs
@@ -0,0 +1,15 @@
+namespace Programming1_FullNameOnAMessageBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("John Lloyd Asenjo Jasmines");
+        }
+    }
+}
